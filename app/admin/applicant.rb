05cbc2b3d0add853e3c1689aa31ba2f229c2e9c0
Scott Kacyn <scott.kacyn@gmail.com>
2015-01-30 13:08:26 -0600

    add resume link to admin

--- app/admin/applicant.rb
+++ app/admin/applicant.rb
@@ -2,6 +2,21 @@ ActiveAdmin.register Applicant do
 
   permit_params :name, :email, :phone, :company, :address, :city, :state, :zip
 
+  index do
+    column :name
+    column :email
+    column :phone
+    column :company
+    column :address
+    column :city
+    column :state
+    column :zip
+    column :resume do |job|
+      link_to "Link", job.resume.url
+    end
+    actions
+  end
+
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
