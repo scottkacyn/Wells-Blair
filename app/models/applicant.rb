8e930517b742b5077dc9ea44fd629adfdd325689
Scott Kacyn <scott.kacyn@gmail.com>
2015-02-05 10:56:16 -0600

    Added word doc support for application uploads

--- app/models/applicant.rb
+++ app/models/applicant.rb
@@ -3,7 +3,7 @@ class Applicant < ActiveRecord::Base
 	belongs_to :job
 	
 	has_attached_file :resume
-	validates_attachment :resume, content_type: { content_type: "application/pdf" }
+	validates_attachment :resume, content_type: { content_type: %w(application/pdf application/msword application/vnd.openxmlformats-officedocument.wordprocessingml.document) }
 	validates :name, :email, :resume, presence: true
 
 end
