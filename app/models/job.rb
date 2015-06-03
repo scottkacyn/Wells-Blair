cbf9fe416a6966d3a7e405c9d3ed62f22a1c37bc
Scott Kacyn <scott.kacyn@gmail.com>
2015-06-03 09:47:13 -0500

    Add code to recreate jobs

--- app/models/job.rb
+++ app/models/job.rb
@@ -8,4 +8,18 @@ class Job < ActiveRecord::Base
 		self.description ||= '<p>Wells Blair, Ltd. is a specialized tax recruitment firm with markets in Chicago, IL and Houston, TX.  We align with tax professionals seeking the next step in their career.  If you are interested in this opportunity or are interested in discussing the current market and your interests, please submit your information below.</p>'
 	end
 
+	def recreate_job
+		applicants = self.applicants
+		new_job = self.dup
+		if new_job.save
+			applicants.update_all(job_id: new_job.id)
+			self.destroy
+		end
+	end
+
+	def self.recreate_jobs
+		Job.all.each do |job|
+			job.recreate_job
+		end
+	end
 end
